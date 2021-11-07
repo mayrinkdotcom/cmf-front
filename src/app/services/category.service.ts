246c04b782d4cfe0a4d85058dce30ac5f188125a
Vinicius Levi <levi.vinicius@yahoo.com.br>
2021-11-07 18:55:37 -0300

    Adicionando editar e remover categoria

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -44,6 +44,38 @@ export class CategoryService {
     }
   }
 
+  async updateCategory(editedCategory: Category) {
+    const url = `${environment.BASE_URL}/categoria/atualizar`;
+
+    const body: Category = editedCategory;
+
+    try {
+      const response = await this.http
+      .put<CategoryResponse>(url, body)
+      .toPromise();
+
+      return response;
+    } catch (error) {
+      this.logError(error);
+      throw error;
+    }
+  }
+
+  async deleteCategory(categoryId: number) {
+    const url = `${environment.BASE_URL}/categoria/deletar/{id}?id=${categoryId}`;
+
+    try {
+      const response = await this.http
+      .delete<null>(url)
+      .toPromise();
+
+      return response;
+    } catch (error) {
+      this.logError(error);
+      throw error;
+    }
+  }
+
   logError(error: Error) {
     console.error('ERROR on category-service:', error);
   }
