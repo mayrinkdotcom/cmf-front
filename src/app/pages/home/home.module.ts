6eb8365eca2688851adc4457e8df3d9324b5c65c
Anna Laura Reis Dornelas <annalaura.rdg@gmail.com>
2021-11-28 15:15:47 -0300

    gráfico front

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { GraficoRendimentoComponent } from 'src/app/core/components/grafico-rendimento/grafico-rendimento.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ChartsModule,
     FormsModule,
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, GraficoRendimentoComponent],
 })
 export class HomePageModule {}
